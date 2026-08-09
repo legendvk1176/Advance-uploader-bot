a065311c51e762442987aac88bf4ffea2ccb5fa6
vishalkumar27440 <vishalkumar27440@gmail.com>
2026-08-09 19:00:04 +0530

    config.py
    Updated bot_token with a new value.

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 api_id =  21595709
 api_hash  = '6b683b86a90c6fae0fbe50a6494bdd53'
-bot_token =  ''
+bot_token =  '8972420117:AAGi-9UKUKWc-d2M7zpjKLYVR2iddQVz-AM'
 LOG =  -1003924837795
 
