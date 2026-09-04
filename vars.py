953aa81700b52d57f2ebb0aa8512f63ee2bf2117
legendvk1176 <legendvk1176@gmail.com>
2026-09-04 23:02:43 +0530

    vars.py
    Updated BOT_TOKEN to a new value.

--- vars.py
+++ vars.py
@@ -2,4 +2,4 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "21595709"))
 API_HASH = environ.get("API_HASH", "6b683b86a90c6fae0fbe50a6494bdd53")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8972420117:AAGi-9UKUKWc-d2M7zpjKLYVR2iddQVz-AM")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8919674457:AAFoqHcf-ywkOCxJJupfEenU2C0LC4hYEWU")
