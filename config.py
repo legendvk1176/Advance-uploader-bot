a13d8f799ab7622eb0830e1c0bc1278dd8b2676e
legendvk1176 <legendvk1176@gmail.com>
2026-09-05 18:57:48 +0530

    config.py
    Updated the bot token in the configuration file.

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 api_id =  21595709
 api_hash  = '6b683b86a90c6fae0fbe50a6494bdd53'
-bot_token =  '8919674457:AAFoqHcf-ywkOCxJJupfEenU2C0LC4hYEWU'
+bot_token =  '8919674457:AAHeq8wpEkTTPRrUqv9yCB1GjFOGf1wbwto'
 LOG =  -1003924837795
 
